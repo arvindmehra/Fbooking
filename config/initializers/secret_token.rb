67fb60fabb11f4ee365c64bb0f2913c1533e6a1a
arvind mehra <bloomberg490@gmail.com>
2013-08-15 14:38:43 +0530

    secret token added

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,3 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+Fbooking::Application.config.secret_token = '4f860ebc9f179c404a641679731510cc3417d68cd4a7b131816ff3355999a4f4b68d52a75c42d5f8abbbed72a9558b9043292f6d552e233c33124ba2330b59a3'
