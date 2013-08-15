5f2f0739b368b435e6113232f7d4610b3f41b992
arvind mehra <bloomberg490@gmail.com>
2013-08-15 14:08:12 +0530

    remove token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Fbooking::Application.config.secret_token = '758f46038dea3057497ee46f59b405c08eea1bfebb304057d2581cd32117f6ad3b32a8b94adc679e0af07ce08f2990a19d4f7f180a52c03efe269777702f0838'
+#Fbooking::Application.config.secret_token = '758f46038dea3057497ee46f59b405c08eea1bfebb304057d2581cd32117f6ad3b32a8b94adc679e0af07ce08f2990a19d4f7f180a52c03efe269777702f0838'
